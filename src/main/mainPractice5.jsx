db143fd10ca3448aba8c64cbd1514e1ecacda209
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-26 14:53:16 +0700

    update project

---
+++ src/main/mainPractice5.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppPractice5 from "../app/AppPractice5";
+
+ReactDOM.createRoot(document.getElementById("root5")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <AppPractice5 />
+        </BrowserRouter>
+    </React.StrictMode>
+);
