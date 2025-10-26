c26c7f274d019a9187a54541671ca52bd05ac148
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-26 15:08:21 +0700

    update project

--- src/main/mainPractice6.jsx
+++ src/main/mainPractice6.jsx
@@ -5,7 +5,8 @@ import AppPractice6 from "../app/AppPractice6";
 
 ReactDOM.createRoot(document.getElementById("root6")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/index">
+        {/* bình thường ko có basename nhưng vì làm chung project và sắp xếp khác nhau nên mới thêm để có thể link đc đến file index6.html */}
+        <BrowserRouter basename="/index/index6.html">
             <AppPractice6 />
         </BrowserRouter>
     </React.StrictMode>
